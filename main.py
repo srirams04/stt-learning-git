36d1ba21e7932053a35145518e3adb56bdcb9471
Sriram Srinivasan <sriram4.srinivasan@gmail.com>
2025-01-09 10:37:15 +0530

    Updated main V1

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import numpy
 class Rectangle:
     def __init__(self, length, breadth):
         self.length = length
-	self.breadth = breadth
-
+        self.breadth = breadth
 
 
     def area(self):
